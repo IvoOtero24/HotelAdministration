501eadea57559576ee8df1f68e6bc0d08cd8b46c
IvoOtero <wi19b080@technikum-wien.at>
2021-07-04 15:01:38 +0200

    Projektdateien hinzufügen.

---
+++ HotelAdministration/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace HotelAdministration.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+        public MainViewModel Main => SimpleIoc.Default.GetInstance<MainViewModel>();
+
+    }
+}
