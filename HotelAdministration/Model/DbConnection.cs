792f6a05d8638b6f283df2f5bd3d210098c1aa42
IvoOtero <wi19b080@technikum-wien.at>
2021-07-07 17:09:07 +0200

    DONE: Struktur, TODO: Verbindung zur (bestehende!) db

---
+++ HotelAdministration/Model/DbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Oracle.ManagedDataAccess.Client;
+
+namespace HotelAdministration.Model
+{
+    public class DbConnection
+    {
+        private OracleConnectionStringBuilder builder = new OracleConnectionStringBuilder("jdbc:oracle:thin:@infdb.technikum-wien.at:1521:o10");
+        public DbConnection()
+        {
+            builder.UserID = "s21bwi4_wi19b080";
+            builder.Password = "00992412Ivo";
+
+            using OracleConnection connection = new OracleConnection(builder.ToString());
+            connection.Open();
+        }
+    }
+}
