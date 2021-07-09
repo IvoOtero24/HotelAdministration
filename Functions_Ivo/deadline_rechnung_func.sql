6a46a64b7b3fd712d006c0bd0d2aac268160b8a4
IvoOtero <wi19b080@technikum-wien.at>
2021-07-09 16:50:27 +0200

    small changes

--- Functions_Ivo/deadline_rechnung_func.sql
+++ Functions_Ivo/deadline_rechnung_func.sql
@@ -1,9 +1,9 @@
 -------------------------------
 --Function 1: Deadline-Rechnung
 -------------------------------
-
-CREATE OR REPLACE TYPE DeadlineTableObject AS OBJECT
+CREATE OR REPLACE TYPE DeadlineTableObj AS OBJECT
 (
+  RechnungsID VARCHAR(50),
   Rechnungssumme NUMBER(7,2),
   Datum DATE,
   AusstellerID NUMBER,
@@ -12,27 +12,26 @@ CREATE OR REPLACE TYPE DeadlineTableObject AS OBJECT
 /
 
 --Create a type of your object 
-CREATE OR REPLACE TYPE DeadlineTable AS TABLE OF DeadlineTableObject;
+CREATE OR REPLACE TYPE DeadlineTable AS TABLE OF DeadlineTableObj;
 /
 
 
 -- Function 
 CREATE OR REPLACE Function DeadlineRechnung
-  (selected_date IN DATE)
   RETURN DeadlineTable
   PIPELINED
 AS 
   CURSOR return_cursor
   IS
-    SELECT Rechnungssumme, Ausstellungsdatum, AusstellerkontoID, EmpfaengerkontoID
+    SELECT RechnungsID, Rechnungssumme, Ausstellungsdatum, AusstellerkontoID, EmpfaengerkontoID
     FROM Rechnung
-    WHERE Ausstellungsdatum = selected_date
-    AND AusstellerKontoID = 500;
+    WHERE (sysdate - Ausstellungsdatum) > 15
+    AND EmpfaengerKontoID = 500;
     
   BEGIN
     FOR i IN return_cursor
     LOOP
-      PIPE ROW (DeadlineTableObject(i.Rechnungssumme, i.Ausstellungsdatum, i.AusstellerkontoID, i.EmpfaengerkontoID));
+      PIPE ROW (DeadlineTableObj(i.RechnungsID, i.Rechnungssumme, i.Ausstellungsdatum, i.AusstellerkontoID, i.EmpfaengerkontoID));
       EXIT WHEN return_cursor%NOTFOUND;
       
       END LOOP; 
@@ -43,7 +42,9 @@ AS
 /
 
 
-SELECT * FROM TABLE(DeadlineRechnung(to_date('22.06.2021','DD.MM.YYYY')));
+SELECT * FROM TABLE(DeadlineRechnung);
+
+SELECT * FROM Rechnung;
 
       
 
