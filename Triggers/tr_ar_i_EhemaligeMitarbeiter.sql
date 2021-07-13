f68591c61cf1b3698b5d706c8ed5628d85da93be
Meishencorona1 <66527728+Meishencorona1@users.noreply.github.com>
2021-07-13 20:10:36 +0200

    Update tr_ar_i_EhemaligeMitarbeiter.sql

--- Triggers/tr_ar_i_EhemaligeMitarbeiter.sql
+++ Triggers/tr_ar_i_EhemaligeMitarbeiter.sql
@@ -1,3 +1,17 @@
+/*********************************************************************
+/**
+/** Trigger: tr_ar_i_EhemaligeMitarbeiter
+/** Type: after row
+/** Type Extension: delete
+/** Developer: Group 3
+/** Description: Wenn ein/e MitarbeiterIn nicht mehr beim Hotel arbeitet, soll man ihn/sie von der Tabelle „Personal“ löschen. 
+/**              Dann soll er/sie zu der Tabelle „EhemaligeMitarbeter“ verschoben werden, damit die anderen Daten von ihm/ihr, 
+/**              z.B. die Rechnungen, die er/sie ausgestellt hat, nicht gelöscht werden müssen und später noch gefunden werden können. 
+/**              Und in der Tabelle soll dokumentiert werden, wann der Mitarbeiter das Hotel verlassen hat.
+/**
+/*********************************************************************
+
+
 set serveroutput on;
 /
 
@@ -12,7 +26,6 @@ BEGIN
   --DELETE FROM rechnung WHERE Rechnung.AusstellerkontoID= :OLDI.kontoID OR Rechnung.EmpfaengerkontoID= :OLDI.kontoID ;
   INSERT INTO  EhemaligeMitarbeiter (PersonID,	GehaltsstufeID,	KontoID, AbteilungsID, Austrittsdatum) 
      VALUES (:OLDI.PERSONID, :OLDI.GEHALTSSTUFEID, :OLDI.KONTOID, :OLDI.ABTEILUNGSID, to_date(sysdate));
-  --COMMIT;
 END;
 /
 
