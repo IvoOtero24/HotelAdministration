66b34ae12d2dbbc403eb558914d2c4497e728c16
Meishencorona1 <66527728+Meishencorona1@users.noreply.github.com>
2021-07-13 20:21:51 +0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,7 +1,10 @@
-
-----------------------
--- PersonalOverView
-----------------------
+/*********************************************************************
+/**
+/** Table: PersonalOverview
+/** Developer: Group 3
+/** Description: Grundsätzliche Informationen der Mitarbeiter 
+/**
+/*********************************************************************/
 
 CREATE OR REPLACE VIEW PersonalOverview AS 
 SELECT Person.vorname, Person.nachname, Adresse.Strasse || ' , ' || Adresse.HausNr as Adresse, Abteilung.Beschreibung AS Abteilung, Gehaltsstufe.Monatsgehalt, Kontoid
@@ -11,9 +14,13 @@ INNER JOIN Gehaltsstufe USING (GehaltsstufeID)
 INNER JOIN Abteilung USING (AbteilungsID) 
 INNER JOIN Adresse USING (AdressID);
 
-------------------------
--- BestellungenOverview
-------------------------
+/*********************************************************************
+/**
+/** Table: BestellungenOverview
+/** Developer: Group 3
+/** Description: Überblick dafür, welche und wie viele Produkte das Hotel mit welchem Preis wann von wem gekauft hat
+/**
+/*********************************************************************/
 
 CREATE OR REPLACE VIEW BestellungenOverview AS
 SELECT Ware.WarenName AS Ware, WarenArt.WarenArtName as Waren_Art, Bestelldetails.Anzahl, Bestelldetails.Preis, Rechnung.Ausstellungsdatum, 
@@ -25,9 +32,13 @@ INNER JOIN WarenArt USING (WarenArtID)
 INNER JOIN Lieferanten USING (LieferantenID)
 INNER JOIN Rechnung USING (RechnungsID);
 
-------------------------
--- GastView
-------------------------
+/*********************************************************************
+/**
+/** Table: GastOverview
+/** Developer: Group 3
+/** Description: Wer aus welchem Land hat wie viele Buchungen bei dem Hotel gemacht.
+/**
+/*********************************************************************/
 
 CREATE OR REPLACE VIEW GastOverview AS
 SELECT Person.vorname, Person.nachname, Adresse.Ort, Land.Landname, ReisepassID, COUNT(*) as Anzahl
@@ -38,9 +49,13 @@ INNER JOIN Adresse USING (AdressID)
 INNER JOIN Land USING (LandID)
 GROUP BY PersonID, Person.vorname, Person.nachname, Adresse.Ort, Land.Landname, ReisepassID;
 
-------------------------
--- KundenbewertungView
-------------------------
+/*********************************************************************
+/**
+/** Table: BewertungenOverview
+/** Developer: Group 3
+/** Description: Grundinformationen für die Bewertungen
+/**
+/*********************************************************************/
 
 CREATE OR REPLACE VIEW BewertungenOverview AS
 SELECT Person.vorname, Person.nachname, Zimmerbuchung.Buchungsdatum, Zimmer.ZimmerArt, Zimmer.ZimmerNr as Zimmer_NR, Bewertungskommentar , Bewertung
