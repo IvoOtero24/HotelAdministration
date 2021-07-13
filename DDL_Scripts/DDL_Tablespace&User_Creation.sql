9304568ea686bbd41b4a0cd6dc0c5c4312797098
SatBar10 <72610546+SatBar10@users.noreply.github.com>
2021-07-13 19:25:24 +0200

    deleted comment-lines

--- DDL_Scripts/DDL_Tablespace&User_Creation.sql
+++ DDL_Scripts/DDL_Tablespace&User_Creation.sql
@@ -1,14 +1,14 @@
 /*SQL Statements for creation and arrangment of DB Schema*/
 
 -- create user
---CREATE USER hotel_admin IDENTIFIED BY "hoteladmin";
+CREATE USER hotel_admin IDENTIFIED BY "hoteladmin";
 
  -- grant user rights
---GRANT CONNECT, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE TRIGGER 
+GRANT CONNECT, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE TRIGGER 
 --	TO hotel_admin;
 
 -- see if user is there
---SELECT * FROM dba_users WHERE username='HOTEL_ADMIN';
+SELECT * FROM dba_users WHERE username='HOTEL_ADMIN';
 
 -- create tablespace for hotel administrator
 CREATE TABLESPACE hotel 
@@ -16,7 +16,7 @@ CREATE TABLESPACE hotel
 	size 10 M 
 	autoextend on;
 	
---ALTER USER hotel_admin DEFAULT TABLESPACE hotel;
+ALTER USER hotel_admin DEFAULT TABLESPACE hotel;
 
 -- check for newly created tabelspace
 SELECT * FROM USER_TABLESPACES;
